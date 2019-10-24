f2a0fcdb7ae184bda9047b74449261bcfdd5a494
YwJiang <rain1999jyw@163.com>
2019-10-24 13:33:14 +0800

    update text classification

--- demos/text_classification/code/text_preprocessing.py
+++ demos/text_classification/code/text_preprocessing.py
@@ -97,12 +97,70 @@ def text_df_to_array_pad(df, text_colname="fact_reason", pad_len=None):
     return all_data, len(word_dictionary), max_len
 
 
+def select_more_than_ten(X, y):
+    # X, y should be dataframe
+    # filter to select y with more than ten labels'
+    X = pd.DataFrame(X)
+    y = pd.DataFrame(y)
+    y_colname = y.columns
+    X_colname = X.columns
+    con_df = pd.concat([X, y], axis=1)
+    train_df = con_df.groupby([i for i in y_colname])
+    train_df_new = train_df.filter(lambda x: x[X_colname].count()>10)
+    return train_df_new[X_colname], train_df_new[y_colname]
+
+
+def X_y_filter_to_array_pad(X, y, pad_len=None):
+    X, y_label = select_more_than_ten(X, y)
+    clean_sent = ['']*len(y)
+    x_list = X.iloc[:, 0].tolist()
+    for i in range(len(x_list)):
+        sent = x_list[i]
+        try:
+            clean_sent[i] = jieba_cut(sent)
+        except:
+            print(sent)
+            print("dataset line: ", i)
+
+    # sentence_to_dict_index
+    word_dictionary, _ = build_dictionary(clean_sent)
+    all_sentence_index = encode_sent(x_list, word_dictionary)
+
+    #
+    if pad_len:
+        max_len = pad_len
+    else:
+        len_list = [0]*len(all_sentence_index)
+        for sentence in all_sentence_index:
+            len_list.append(len(sentence))
+        max_len = max(len_list)
+
+    all_data = keras.preprocessing.sequence.pad_sequences(all_sentence_index,
+                                                          value=word_dictionary['<PAD>'],
+                                                          padding='post',
+                                                          maxlen=max_len)
+    return all_data, y_label, len(word_dictionary), max_len
+
+
+
+
+
+
 if __name__ == "__main__":
     print(__name__)
     path = "../data/credit_card_labels_factreason.xlsx"
     pd_all = pd.read_excel(path)
-    df, dict_len, padding_len = text_df_to_array_pad(pd_all, 'fact_reason', pad_len=100)
+    # df, dict_len, padding_len = text_df_to_array_pad(pd_all, 'fact_reason', pad_len=100)
+    #     # df = pd.DataFrame(df)
+    #     # print("df shape: ", df.shape)
+    #     # print("dictionary: ", dict_len)
+    #     # print("padding: ", padding_len)
+
+    X = pd_all['fact_reason']
+    y = pd_all.iloc[:, 0:3]
+    df, y, dict_len, padding_len = X_y_filter_to_array_pad(X, y, pad_len=100)
     df = pd.DataFrame(df)
     print("df shape: ", df.shape)
-    print("dictionary: ", dict_len)
+    print("y shape: ", y.shape)
+    print("dictionary len: ", dict_len)
     print("padding: ", padding_len)
